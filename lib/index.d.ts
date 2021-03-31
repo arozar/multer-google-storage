0dc7f8546c92116855b3120684c32968440c0dfd
Chao Chen <chao.chen@takiwa.co>
2021-04-01 10:52:25 +1300

    npm run clean & npm run build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,15 @@
 import multer = require('multer');
-import { StorageOptions } from '@google-cloud/storage';
+import { PredefinedAcl, StorageOptions } from '@google-cloud/storage';
 import { Request } from 'express';
+export declare type MulterGoogleCloudStorageOptions = {
+    acl?: PredefinedAcl;
+    bucket?: string;
+    contentType?: ContentTypeFunction | string;
+    destination?: any;
+    filename?: any;
+    hideFilename?: boolean;
+    uniformBucketLevelAccess?: boolean;
+};
 export default class MulterGoogleCloudStorage implements multer.StorageEngine {
     private gcsBucket;
     private gcsStorage;
@@ -9,22 +18,9 @@ export default class MulterGoogleCloudStorage implements multer.StorageEngine {
     getDestination(req: any, file: any, cb: any): void;
     getContentType(req: any, file: any): any;
     private getBlobFileReference;
-    constructor(opts?: StorageOptions & {
-        bucket?: string;
-        destination?: any;
-        filename?: any;
-        hideFilename?: boolean;
-        contentType?: ContentTypeFunction;
-    });
+    constructor(opts?: StorageOptions & MulterGoogleCloudStorageOptions);
     _handleFile: (req: any, file: any, cb: any) => void;
     _removeFile: (req: any, file: any, cb: any) => void;
 }
-export declare function storageEngine(opts?: StorageOptions & {
-    bucket?: string;
-    destination?: any;
-    filename?: any;
-    hideFilename?: boolean;
-    contentType?: ContentTypeFunction;
-    uniformBucketLevelAccess?: boolean;
-}): MulterGoogleCloudStorage;
+export declare function storageEngine(opts?: StorageOptions & MulterGoogleCloudStorageOptions): MulterGoogleCloudStorage;
 export declare type ContentTypeFunction = (req: Request, file: Express.Multer.File) => string | undefined;
